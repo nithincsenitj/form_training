ed3803dfb0bab078e677845f8fbac06aff355a1d
nithinchandrank <nithincsenitj@gmail.com>
2018-10-18 11:19:50 +0530

    got credentials time to match

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
 app.get('/', function (req, res) {
    res.sendFile( __dirname + "/" + "htmlform.html" );
 })
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      email:req.body.email,
+      password:req.body.password
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
 
 var server = app.listen(8081, function () {
    var host = server.address().address
